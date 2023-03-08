309de79b54010dd3e1287dccd2167b38296cc763
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-03-08 10:24:33 -0800

    creating new users

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
-  // console.log(response.data);
+  console.log(response.data);
 
   //! DEV ONLY!!!
   await pause(1000);
