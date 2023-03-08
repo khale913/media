41bb886e969e14b1e9bdd9f08c51e8a3805f1ba9
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-03-08 12:28:26 -0800

    deleting user

--- src/store/thunks/removeUser.js
+++ src/store/thunks/removeUser.js
@@ -2,10 +2,11 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const removeUser = createAsyncThunk("users/remove", async (user) => {
-  const response = await axios.delete(`http://localhost:3005/users/${user.id}`);
+  console.log(user.id);
+  await axios.delete(`http://localhost:3005/users/${user.id}`);
 
   // FIX !!!
-  return response.data;
+  return user;
 });
 
 export { removeUser };
