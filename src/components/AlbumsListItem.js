a1f4f208d995742bcbc77e6c2d31b884c88c475b
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-03-08 18:03:21 -0800

    better tag implementation

--- src/components/AlbumsListItem.js
+++ src/components/AlbumsListItem.js
@@ -1,15 +1,26 @@
 import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
 import { GoTrashcan } from "react-icons/go";
+import { useRemoveAlbumMutation } from "../store";
 
 function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+
   const header = (
-    <div>
-      <Button>
+    <>
+      <Button
+        className="mr-2"
+        loading={results.isLoading}
+        onClick={handleRemoveAlbum}
+      >
         <GoTrashcan />
       </Button>
       {album.title}
-    </div>
+    </>
   );
   return (
     <ExpandablePanel key={album.id} header={header}>
--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -20,6 +20,9 @@ const albumsApi = createApi({
   endpoints(builder) {
     return {
       removeAlbum: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "Album", id: album.id }];
+        },
         query: (album) => {
           return {
             method: "DELETE",
@@ -29,7 +32,7 @@ const albumsApi = createApi({
       }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          return [{ type: "UsersAlbums", id: user.id }];
         },
         query: (user) => {
           return {
@@ -44,7 +47,11 @@ const albumsApi = createApi({
       }),
       fetchAlbums: builder.query({
         providesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          const tags = result.map((album) => {
+            return { type: "Album", id: album.id };
+          });
+          tags.push({ type: "UsersAlbums", id: user.id });
+          return tags;
         },
         query: (user) => {
           return {
